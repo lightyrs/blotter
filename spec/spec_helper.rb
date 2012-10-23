b13a308706004006f552455cc85ba593356bbe9a
Harris Novick <harris@harrisnovick.com>
2012-10-22 21:38:09 -0400

    Remove deprecation notices

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/blotter'
 require_relative 'rails/facebook_page'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.mock_with :rr
   # or if that doesn't work due to a version incompatibility
   # config.mock_with RR::Adapters::Rspec
