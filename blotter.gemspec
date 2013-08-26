0efd4607a0c0e3f6a96620b49a38126acb066ae7
Harris Novick <harrisdavidnovick@gmail.com>
2013-08-26 01:51:52 -0400

    working version for rails 3

--- blotter.gemspec
+++ blotter.gemspec
@@ -18,8 +18,8 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_runtime_dependency "activesupport", "~> 3.2.8"
-  spec.add_runtime_dependency "actionpack", "~> 3.2.8"
+  spec.add_runtime_dependency "activesupport", "~> 3.2"
+  spec.add_runtime_dependency "actionpack", "~> 3.2"
   spec.add_runtime_dependency "koala", "~> 1.5.0"
 
   spec.add_development_dependency "bundler", "~> 1.3"
--- lib/blotter/railtie.rb
+++ lib/blotter/railtie.rb
@@ -1,23 +1,22 @@
 module Blotter
   class Railtie < Rails::Railtie
-    initializer "application_controller.initialize_blotter" do
-      module ActiveRecord
-        class Base
-          def self.blotter(blotter_method)
-            blotter_model = caller[0][/`<class:([^']*)>'/, 1]
-            Blotter.register_blotter_model(blotter_model.safe_constantize)
-            Blotter.register_blotter_method(blotter_method)
-          end
+    initializer "blotter_railtie.initialize_blotter" do
+      module BlotterActiveRecordExt
+        def blotter(blotter_method)
+          blotter_model = caller[0][/`<class:([^']*)>'/, 1]
+          Blotter.register_blotter_model(blotter_model.safe_constantize)
+          Blotter.register_blotter_method(blotter_method)
         end
       end
+      ActiveRecord::Base.extend(BlotterActiveRecordExt)
 
-      module BlotterExtensions
+      module BlotterActionControllerExt
         def blotter(controller_actions = {})
           Blotter.register_controller_actions(controller_actions)
           include Blotter
         end
       end
-      ActionController::Base.extend(BlotterExtensions)
+      ActionController::Base.extend(BlotterActionControllerExt)
     end
   end
 end
